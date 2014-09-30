6d9fdebd118a34be127130be61212626cc6ea4c0
Mark Watkin <mwat952@aucklanduni.ac.nz>
2014-09-30 13:23:13 +1300

    Added MultiPlatformInputs Enum containing example possible input types

---
+++ Assets/Scripts/Input/MultiPlatformInputs.cs
@@ -0,0 +1,16 @@
+﻿public enum MultiPlatformInputs {
+	//TouchScreen Inputs
+	SwipeUp,
+	SwipeDown,
+	SwipeRight,
+	SwipeLeft,
+	SwipeUpRight,
+	SwipeUpLeft,
+	SwipeUpRightDown,
+	
+	//KeyBoard Inputs
+	UpArrow,
+	DownArrow,
+	RightArrow,
+	LeftArrow
+}
