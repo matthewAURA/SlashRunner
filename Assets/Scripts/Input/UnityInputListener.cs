298101caea8bfc71c1294046d6c70935479b2e97
Mark Watkin <mwat952@aucklanduni.ac.nz>
2014-10-02 11:37:26 +1300

    Added input listener for keyboard events when testing in unity

---
+++ Assets/Scripts/Input/UnityInputListener.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityInputListener : MonoBehaviour {
+
+	private InputMap inputMap = InputMap.getInputMap();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.WindowsEditor) {
+			if (Input.GetAxis ("Vertical") > 0) {
+				inputMap.FireInputEvents(MultiPlatformInputs.UpArrow);
+			} else if (Input.GetAxis ("Vertical") > 0) {
+				inputMap.FireInputEvents(MultiPlatformInputs.DownArrow);
+			} else if (Input.GetAxis ("Horizontal") > 0) {
+				inputMap.FireInputEvents(MultiPlatformInputs.RightArrow);
+			} else if (Input.GetAxis ("Horizontal") < 0) {
+				inputMap.FireInputEvents(MultiPlatformInputs.LeftArrow);
+			}
+		}
+	}
+}
