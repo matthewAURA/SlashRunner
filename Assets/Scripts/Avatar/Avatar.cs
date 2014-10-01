45d00fa33d860f2da56124e77302404563916e5f
Mark Watkin <mwat952@aucklanduni.ac.nz>
2014-10-02 12:47:48 +1300

    Added skeleton code for avatar <@LordMark | @bert-huang>

---
+++ Assets/Scripts/Avatar/Avatar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Avatar : MonoBehaviour {
+
+	private InputMap inputMap;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("I am here");
+		inputMap = InputMap.getInputMap();
+		inputMap.Add(MultiPlatformInputs.UpArrow, jump);
+		inputMap.Add(MultiPlatformInputs.SwipeUp, jump);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void jump () {
+		Debug.Log("I am jumpping");
+	}
+}
