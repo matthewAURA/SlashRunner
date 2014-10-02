ba715bab202a541ab216827d3a793d83a99ffde5
Matthew Chiam <mchiam1991@gmail.com>
2014-10-02 18:26:05 +1300

    Added two types of button functionality: one that changes camera position and one that changes scene. Test scene created for former.

---
+++ Assets/Scripts/Interface/Button/LoadSceneButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Loads the given scene when pressed.
+/// </summary>
+public class LoadSceneButton : ButtonBehaviour {
+
+	public string sceneName;
+
+	protected override void OnButtonPress()
+	{
+		Application.LoadLevel(sceneName);
+	}
+}
