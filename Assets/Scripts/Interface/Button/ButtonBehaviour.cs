aebbfd6241d6603d15dabb9b821c984d13a2207f
Matthew Chiam <mchiam1991@gmail.com>
2014-10-02 18:26:05 +1300

    Added two types of button functionality: one that changes camera position and one that changes scene. Test scene created for former.

---
+++ Assets/Scripts/Interface/Button/ButtonBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents a component that performs some action when pressed.
+/// Subclasses should implement specific functionality. 
+/// </summary>
+public abstract class ButtonBehaviour : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		OnButtonPress ();
+	}
+
+	protected abstract void OnButtonPress();
+}
