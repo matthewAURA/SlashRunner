5b5e48dd3d12e93dc45a393be59c6deaba78b2ed
Tommy <ngo.tommy@outlook.com>
2014-10-03 11:14:02 +1300

    add multiple combo to the HUDScript

--- Assets/Scripts/Score/HUDScript.cs
+++ Assets/Scripts/Score/HUDScript.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class HUDScript : MonoBehaviour {
 
 	private float playerScore = 0;
-	
+	private int multiple = 1;
 	void Update () {
-		playerScore += Time.deltaTime;
+		playerScore += Time.deltaTime * multiple;
 	}
 	
 	public void IncreaseScoreByCollectable(int amount){
 		playerScore += amount;
 	}
+
+	public void SetMultiple(int multiple){
+		this.multiple = multiple;
+	}
+
 	
 	void OnGUI(){
 		GUI.Label (new Rect (0, 0, 100, 30), "Score: " + (int)(playerScore * 100));
