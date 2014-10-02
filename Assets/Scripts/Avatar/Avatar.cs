7055058c68718d58b78f66eb1e631fbb917bf2dd
Bert Huang <bert.huang@outlook.com>
2014-10-02 13:39:54 +1300

    Added logic for ground detection.

--- Assets/Scripts/Avatar/Avatar.cs
+++ Assets/Scripts/Avatar/Avatar.cs
@@ -4,19 +4,37 @@ using System.Collections.Generic;
 
 public class Avatar : MonoBehaviour {
 
+	[SerializeField] LayerMask whatIsGround = 0;
+	Transform groundCheck;								
+	float groundedRadius = 0.2f;	
+	bool grounded = false;
+
 	private InputMap inputMap;
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log("I am here");
+
+	void Awake() {
 		inputMap = InputMap.getInputMap();
 		inputMap.Add(MultiPlatformInputs.UpArrow, jump);
 		inputMap.Add(MultiPlatformInputs.SwipeUp, jump);
+		groundCheck = transform.Find ("GroundCheck");
 	}
-	
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void FixedUpdate() {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (grounded) {
+			Debug.Log ("I am on the ground");
+		} else {
+			Debug.Log ("Not on the ground");
+		}
 	}
 
 	public void jump () {
