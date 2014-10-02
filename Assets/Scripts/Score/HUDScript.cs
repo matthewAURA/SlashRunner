fa889964ffa020308717b7f2c599dbb297c9ead4
Tommy <ngo.tommy@outlook.com>
2014-10-03 10:26:49 +1300

    add scoring system into the game. ONLY has score for endless runner mode. Does not have multiple score.

---
+++ Assets/Scripts/Score/HUDScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+	private float playerScore = 0;
+	
+	void Update () {
+		playerScore += Time.deltaTime;
+	}
+	
+	public void IncreaseScoreByCollectable(int amount){
+		playerScore += amount;
+	}
+	
+	void OnGUI(){
+		GUI.Label (new Rect (0, 0, 100, 30), "Score: " + (int)(playerScore * 100));
+	}
+}
