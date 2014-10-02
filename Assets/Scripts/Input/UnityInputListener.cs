8c39460cf18e3e4efe4b13a0b5fe931ccce2f6c0
Mark Watkin <mwat952@aucklanduni.ac.nz>
2014-10-03 00:19:48 +1300

    Edited unity input listener such that only a single event is fired when a key is pressed

--- Assets/Scripts/Input/UnityInputListener.cs
+++ Assets/Scripts/Input/UnityInputListener.cs
@@ -2,25 +2,41 @@
 using System.Collections;
 
 public class UnityInputListener : MonoBehaviour {
-
+	
 	private InputMap inputMap = InputMap.getInputMap();
-
+	
+	private bool keyUp = true;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor) {
+		if (Application.platform == RuntimePlatform.WindowsEditor) {
 			if (Input.GetAxis ("Vertical") > 0) {
-				inputMap.FireInputEvents(MultiPlatformInputs.UpArrow);
+				if(keyUp) {
+					inputMap.FireInputEvents(MultiPlatformInputs.UpArrow);
+					keyUp = false;
+				}
 			} else if (Input.GetAxis ("Vertical") < 0) {
-				inputMap.FireInputEvents(MultiPlatformInputs.DownArrow);
+				if(keyUp) {
+					inputMap.FireInputEvents(MultiPlatformInputs.DownArrow);
+					keyUp = false;
+				}
 			} else if (Input.GetAxis ("Horizontal") > 0) {
-				inputMap.FireInputEvents(MultiPlatformInputs.RightArrow);
+				if(keyUp) {
+					inputMap.FireInputEvents(MultiPlatformInputs.RightArrow);
+					keyUp = false;
+				}
 			} else if (Input.GetAxis ("Horizontal") < 0) {
-				inputMap.FireInputEvents(MultiPlatformInputs.LeftArrow);
+				if(keyUp) {
+					inputMap.FireInputEvents(MultiPlatformInputs.LeftArrow);
+					keyUp = false;
+				}
+			} else {
+				keyUp = true;
 			}
 		}
 	}
