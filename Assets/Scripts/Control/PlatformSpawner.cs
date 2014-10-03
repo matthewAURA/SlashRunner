d62b52d44efdcfbdfe8624475f27827b0883d74e
Matthew Dyer <matthew.dyer@aura.org.nz>
2014-10-04 12:37:57 +1300

    added basic platform that tracks a moving object

---
+++ Assets/Scripts/Control/PlatformSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSpawner : MonoBehaviour {
+
+	/// <summary>
+	/// Object to spawn more instances of
+	/// </summary>
+	public Transform target;
+
+	private float yHeight;
+
+	// Use this for initialization
+	void Start () {
+		yHeight = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target) {
+			var tPos = target.position;
+			tPos.y = yHeight;
+
+			if (tPos.x + target.collider2D.bounds.size.x/2 > transform.position.x+ this.collider2D.bounds.size.x/2){
+				transform.position = tPos;
+			}
+
+
+		}
+	}
+}
