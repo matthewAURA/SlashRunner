2567611c8b361fa3469d183c3406f60e20ab07e0
Matthew Chiam <mchiam1991@gmail.com>
2014-10-04 19:40:23 +1300

    Camera currently scales to device aspect ratio. Needs to be tested.

---
+++ Assets/Scripts/Interface/ScaleCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleCamera : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		camera.aspect = 16f / 9f;
+	}
+}
