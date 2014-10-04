e7735134a2e2cd4ca4a384266c36366be6e4532a
Bert Huang <bert.huang@outlook.com>
2014-10-04 14:10:19 +1300

    Added new sprite for avatar. Created animation and prefab for the new avatar.

--- Assets/Scripts/Avatar/Avatar.cs
+++ Assets/Scripts/Avatar/Avatar.cs
@@ -8,11 +8,13 @@ public class Avatar : MonoBehaviour {
 	Transform groundCheck;								
 	float groundedRadius = 0.2f;	
 	bool grounded = false;
+	bool jumping = false;
 	private bool moving = true;
+	Animator anim;
 
 	private InputMap inputMap;
 	public float jumpForce = 500f;
-	public float movementForce = 10f;
+	public float movementForce = 5f;
 
 
 	void Awake() {
@@ -20,6 +22,7 @@ public class Avatar : MonoBehaviour {
 		inputMap.Add(MultiPlatformInputs.UpArrow, jump);
 		inputMap.Add(MultiPlatformInputs.SwipeUp, jump);
 		groundCheck = transform.Find ("GroundCheck");
+		anim = GetComponent<Animator> ();
 	}
 
 	// Use this for initialization
@@ -29,23 +32,36 @@ public class Avatar : MonoBehaviour {
 
 	void FixedUpdate() {
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundedRadius, whatIsGround);
-
+		Debug.Log (grounded);
 		move ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		if (grounded && jumping) {
+			jumping = false;
+		}
 	}
 
 	public void move() {
 		if (moving) {
-			rigidbody2D.velocity = new Vector2 (movementForce, rigidbody2D.velocity.y);
+			//rigidbody2D.AddForce(new Vector2(movementForce, 0f));
+			if(grounded) {
+				rigidbody2D.velocity = new Vector2 (movementForce, rigidbody2D.velocity.y);
+			}else {
+				if (jumping){
+					rigidbody2D.velocity = new Vector2 (movementForce*0.9f, rigidbody2D.velocity.y);
+				} else {
+					rigidbody2D.velocity = new Vector2 (movementForce*0.3f, rigidbody2D.velocity.y);
+				}
+
+			}
 		}
 	}
 
 	public void jump () {
-		if (grounded) {
+		if (!jumping && grounded) {
+			jumping = true;
 			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
 		}
 	}
