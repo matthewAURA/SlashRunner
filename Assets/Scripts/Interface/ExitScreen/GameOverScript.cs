ad14e714adc334841773a98a2e830e3177ed3dc1
Tommy <ngo.tommy@outlook.com>
2014-10-07 09:56:36 +1300

    Moved Scripts to the appropriate folders

--- Assets/Scripts/Interface/ExitScreen/GameOverScript.cs
+++ Assets/Scripts/Interface/ExitScreen/GameOverScript.cs
@@ -21,16 +21,12 @@ public class GameOverScript : MonoBehaviour {
 		Debug.Log ("IN ON GUI");
 		Debug.Log(scoring.highScores.Count);
 		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "GAME OVER");
-		int y = 70;
+		int y = 100;
 		foreach(var i in scoring.highScores){
 			GUI.Label (new Rect (Screen.width / 2 - 40, y, 100, 40), "High Scores: " + i.name + " " + (int)i.score);
 			y += 40;
 		}
-		GUI.Label( new Rect (Screen.width / 2 - 40, 300, 80, 30), "Score: " + (int)scoreEntry.score);
-
-		if (GUI.Button( new Rect( Screen.width / 2 - 30, 350, 60, 30), "Reply??")){
-			Application.LoadLevel(0);
-		}
+		GUI.Label( new Rect (Screen.width / 2 - 40, Screen.height - 100, 80, 30), "Score: " + (int)scoreEntry.score);
 	}
 
 }
