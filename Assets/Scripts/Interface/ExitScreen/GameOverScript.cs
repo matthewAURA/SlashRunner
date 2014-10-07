404eca077948c374d87f9d7f21fec616fed990e3
Tommy <ngo.tommy@outlook.com>
2014-10-07 20:09:58 +1300

    Having high score screen showing the high score. Game over screen does not show high score anymore

--- Assets/Scripts/Interface/ExitScreen/GameOverScript.cs
+++ Assets/Scripts/Interface/ExitScreen/GameOverScript.cs
@@ -7,26 +7,16 @@ public class GameOverScript : MonoBehaviour {
 	private ScoringSystem.ScoreEntry scoreEntry;
 
 	void Start () {
-		Debug.Log ("Start Gameover");
 		scoreEntry = new ScoringSystem.ScoreEntry ();
-		scoreEntry.score = PlayerPrefs.GetFloat ("ScoreFloat");
-		Debug.Log ((int)scoreEntry.score);
-		scoreEntry.name = PlayerPrefs.GetString ("Name");
 		scoring = gameObject.GetComponent<ScoringSystem>();
-		scoring.InitializeHighScore ();
+		scoreEntry.name = PlayerPrefs.GetString("Name");
+		scoreEntry.score = PlayerPrefs.GetFloat ("ScoreFloat");
 		scoring.AddScoreIntoHighScore (scoreEntry);
 	}
 
 	void OnGUI(){
-		Debug.Log ("IN ON GUI");
-		Debug.Log(scoring.highScores.Count);
 		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "GAME OVER");
-		int y = 100;
-		foreach(var i in scoring.highScores){
-			GUI.Label (new Rect (Screen.width / 2 - 40, y, 100, 40), "High Scores: " + i.name + " " + (int)i.score);
-			y += 40;
-		}
-		GUI.Label( new Rect (Screen.width / 2 - 40, Screen.height - 100, 80, 30), "Score: " + (int)scoreEntry.score);
+		GUI.Label( new Rect (Screen.width / 2 - 40, Screen.height/2 - 40, 80, 30), "Score: " + (int)scoreEntry.score);
 	}
 
 }
---
+++ Assets/Scripts/Interface/HighScoreScreen/HighScoreScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScoreScript : MonoBehaviour {
+	private ScoringSystem scoring;
+	// Use this for initialization
+	void Start () {
+		scoring = gameObject.GetComponent<ScoringSystem>();
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		int y = 100;
+		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 100, 50), "HIGH SCORES:");
+		foreach(var i in scoring.highScores){
+			GUI.Label (new Rect (Screen.width / 2 - 40, y, 100, 40),i.name + " " + (int)i.score);
+			y += 40;
+			if (y > 300){
+				break;
+			}
+		}
+	}
+}
