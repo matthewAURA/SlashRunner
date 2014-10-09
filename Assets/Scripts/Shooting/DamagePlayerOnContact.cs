96e652a587e711fe0cbe4567b90fd6c57b9a2238
Matthew Chiam <mchiam1991@gmail.com>
2014-10-10 09:44:27 +1300

    Added logic that can be used to damage avatar/player on contact

---
+++ Assets/Scripts/Shooting/DamagePlayerOnContact.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents how much damage a player should take on contact
+/// with this object. This object should have both RigidBody2D
+/// and BoxCollider2D attached, and should be a trigger. The player
+/// should handle damage-taken logic, and should also be associated
+/// with a BoxCollider2D.
+/// </summary>
+public class DamagePlayerOnContact : MonoBehaviour
+{
+	/// <summary>
+	/// The damage to be deducted from the player.
+	/// </summary>
+	public int damage = 1;
+}
