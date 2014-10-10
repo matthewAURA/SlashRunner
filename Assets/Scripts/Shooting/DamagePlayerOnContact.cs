de6d8bc0c3d15ac9f1ce75ad0f4a0ab58b921135
Matthew Chiam <mchiam1991@gmail.com>
2014-10-10 14:33:15 +1300

    Added error checking for proper usage of DamagePlayerOnContact.cs

--- Assets/Scripts/Shooting/DamagePlayerOnContact.cs
+++ Assets/Scripts/Shooting/DamagePlayerOnContact.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 /// <summary>
 /// Represents how much damage a player should take on contact
-/// with this object. This object should have both RigidBody2D
+/// with this object. This object should have both Rigidbody2D
 /// and BoxCollider2D attached, and should be a trigger. The player
 /// should handle damage-taken logic, and should also be associated
 /// with a BoxCollider2D.
@@ -14,4 +14,14 @@ public class DamagePlayerOnContact : MonoBehaviour
 	/// The damage to be deducted from the player.
 	/// </summary>
 	public int damage = 1;
+
+	void Start()
+	{
+		if (GetComponent<BoxCollider2D> () == null ||
+			GetComponent<Rigidbody2D> () == null)
+		{
+			throw new MissingComponentException("Must have both BoxCollider2D and Rigidbody2D" +
+			                                    "components attached.");
+		}
+	}
 }
