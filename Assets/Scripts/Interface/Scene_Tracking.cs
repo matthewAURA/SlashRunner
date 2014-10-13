2068d7b951a9dab367655010bd2755ed21714b42
Mark Watkin <mwat952@aucklanduni.ac.nz>
2014-10-13 20:07:02 +1300

    added scene tracking script

---
+++ Assets/Scripts/Interface/Scene_Tracking.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_Tracking : MonoBehaviour {
+
+	private static int previouseScene;
+	private static int currentScene;
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+		currentScene = Application.loadedLevel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.loadedLevel != currentScene) {
+			previouseScene = currentScene;
+			currentScene = Application.loadedLevel;
+		}
+	}
+
+	public static int GetPreviouseScene() {
+		return previouseScene;
+	}
+}
