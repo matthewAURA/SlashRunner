3a571c2e32b2e6feb2e9cbdf47bd356de02c77b8
Bert Huang <bert.huang@outlook.com>
2014-10-14 23:20:13 +1300

    Implemented parallaxing

---
+++ Assets/Scripts/Environmental/BackgroundLoopScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class BackgroundLoopScript {
+
+	public static bool IsVisibleFrom(this Renderer renderer, Camera camera) {
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
