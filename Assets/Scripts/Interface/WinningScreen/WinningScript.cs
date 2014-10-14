aaed9bb6e02fc2b3a3281aa29925483ad5a8ece6
Tommy <ngo.tommy@outlook.com>
2014-10-15 10:51:59 +1300

    Fixed scoring system to store and display high score. Added button for sound effect and music

---
+++ Assets/Scripts/Interface/WinningScreen/WinningScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinningScript : MonoBehaviour {
+	private int score;
+	
+	void Start(){
+		score = PlayerPrefs.GetInt ("ScoreInt");
+	}
+
+	void OnGUI(){
+		GUIStyle style = new GUIStyle();
+		style.normal.textColor = Color.blue;
+		style.fontSize = 80;
+		style.alignment = TextAnchor.MiddleCenter;
+		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "CONGRATULATION",style);
+		style.normal.textColor = Color.white;
+		style.fontSize = 40;
+		GUI.Label( new Rect (Screen.width / 2 - 40, Screen.height/2 - 40, 80, 30), "Score: " + score,style);
+
+	}
+}
