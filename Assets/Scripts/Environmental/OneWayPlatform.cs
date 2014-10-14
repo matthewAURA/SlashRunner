ab45513c664f7dca6a406b6a0910ae0558172645
mli166 <mli166@aucklanduni.ac.nz>
2014-10-15 10:55:07 +1300

    added one way platforms for straight horizontal platforms

--- Assets/Scripts/Environmental/OneWayPlatform.cs
+++ Assets/Scripts/Environmental/OneWayPlatform.cs
@@ -8,29 +8,28 @@ public class OneWayPlatform : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.Find("Avatar").rigidbody2D;
-
-		avatar = GameObject.Find ("Avatar").GetComponent<Avatar> ();
+		GameObject g = GameObject.Find ("Avatar");
+		player = g.rigidbody2D;
+		avatar = g.GetComponent<Avatar> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("grounded: " + avatar.grounded);
-		if(!avatar.grounded && player.velocity.y <= -1){
+		//does not work for sloped platforms
+		if(player.velocity.y <= 0 && this.transform.position.y < player.position.y){
 			activate();
 		}else{
 			deactivate();
 		}
-		//Debug.Log ("feet: " + feet.position.y + " platform: " + this.transform.position.y);
 	}
 
 	void deactivate(){
-		Debug.Log ("deactivate");
+		//Debug.Log ("deactivate");
 		this.collider2D.enabled = false;
 	}
 
 	void activate(){
-		Debug.Log ("activate");
+		//Debug.Log ("activate");
 		this.collider2D.enabled = true;
 	}
 }
