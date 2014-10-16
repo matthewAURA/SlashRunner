3073fb39ffdc8aeb01791cec6cf02d826210fe88
mli166 <mli166@aucklanduni.ac.nz>
2014-10-16 15:13:39 +1300

    added prefabs for one-way(floating) platforms, renamed character prefab to MainCharacter

--- Assets/Scripts/Environmental/OneWayPlatform.cs
+++ Assets/Scripts/Environmental/OneWayPlatform.cs
@@ -5,18 +5,33 @@ public class OneWayPlatform : MonoBehaviour {
 
 	Rigidbody2D player;
 	Avatar avatar;
-
+	float platformTop;
+	float playerFeet;
 	// Use this for initialization
 	void Start () {
-		GameObject g = GameObject.Find ("Avatar");
+		GameObject g = GameObject.Find ("Character");
 		player = g.rigidbody2D;
 		avatar = g.GetComponent<Avatar> ();
+
+		platformTop = this.transform.collider2D.bounds.center.y + this.transform.collider2D.bounds.size.y / 2;
+		/*
+		Debug.Log ("platform height: "+this.transform.collider2D.bounds.size.y);
+		Debug.Log ("platform y: "+this.transform.collider2D.bounds.center.y);
+		Debug.Log ("platform top: " + platformTop);
+
+		float playerFeet = player.collider2D.bounds.center.y - player.collider2D.bounds.size.y / 2;
+		Debug.Log ("player height: "+player.collider2D.bounds.size.y);
+		Debug.Log ("player y: "+player.collider2D.bounds.center.y);
+		Debug.Log ("player feet: " + playerFeet);
+		*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log (avatar.grounded);
 		//does not work for sloped platforms
-		if(player.velocity.y <= 0 && this.transform.position.y < player.position.y){
+		float playerFeet = player.position.y + player.collider2D.bounds.size.y / 2;
+		if(player.velocity.y <= 0 && platformTop < playerFeet){
 			activate();
 		}else{
 			deactivate();
