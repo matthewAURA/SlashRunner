2148fb433620f25ff05bb18dacbf00da61a8800e
Tommy <ngo.tommy@outlook.com>
2014-10-19 19:24:30 +1300

    Make fish an enemy. Added attack range, game object to make it work

--- Assets/Scripts/Environmental/FishScript.cs
+++ Assets/Scripts/Environmental/FishScript.cs
@@ -4,21 +4,12 @@ using System;
 
 public class FishScript : MonoBehaviour {
 
-	public double lowerLimit = 0.5;
-	public double upperLimit = 1;
-	
-	// Used to keep track of when next bounce should be
-	private double cooldown = 0;
-	private System.Random rnd = new System.Random(Guid.NewGuid().GetHashCode());
-	
-	private const int BOUNCE_STRENGTH = 500;
 	private const int GRAVITY_SCALE = 2;
-	private const int X_VELOCITY_MIN = 15;
-	private const int X_VELOCITY_MAX = 25;
 	private bool isDown;
-	private int rotation_x;
-	private int rotation_y;
-	private int rotation_z;
+	private float beginX = -0.5f;
+	private float finalX = -1.5f;
+	private float beginTorque = -0.8f;
+	private float finalTorque = -7;
 
 	// Use this for initialization
 	void Start () {
@@ -28,28 +19,38 @@ public class FishScript : MonoBehaviour {
 		{
 			animator.enabled = true;
 		}
-		
-		// Allow bird start moving
+		if (transform.localScale.y == 0) {
+			beginX *= -1;
+			finalX *= -1;
+			beginTorque *= -1;
+			finalTorque *= -1;
+		}
+
+
+		// Allow fish start moving
 		rigidbody2D.gravityScale = GRAVITY_SCALE;
 		rigidbody2D.velocity = new Vector3 (0, 10, 0);
 		isDown = false;
 		// Destroy birds when well off offscreen
-		Destroy (gameObject, 10);
+		Destroy (gameObject, 5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		Vector3 position = transform.position;
-		if (position.y < 5 && !isDown) {
-			rigidbody2D.velocity = new Vector3 (-0.5f, 10, 0);
-			rigidbody2D.AddTorque(-0.7f);
+
+		if (position.y < 4 && !isDown) {
+			
+			rigidbody2D.velocity = new Vector3 (beginX, 20, 0);
+			rigidbody2D.AddTorque(beginTorque);
 		}else{
 			isDown = true;
 		}
 
 		if (isDown) {
-			rigidbody2D.AddTorque(-4);
-			rigidbody2D.velocity = new Vector3 (-1.5f, -10, 0);
+			rigidbody2D.AddTorque(finalTorque);
+			rigidbody2D.velocity = new Vector3 (finalX, -10, 0);
 		}
 	}
 }
