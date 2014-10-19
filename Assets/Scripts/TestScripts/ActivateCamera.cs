a88788c07e72e79f2b1e4f8a6814b3d5aedf9fc0
Mark Aziz <mazi910@aucklanduni.ac.nz>
2014-10-20 10:42:33 +1300

    Added a camera background functionality

---
+++ Assets/Scripts/TestScripts/ActivateCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateCamera : MonoBehaviour {
+
+	void Start(){
+//		Vector3 scale = transform.localScale;
+//		scale.x = 22;
+//		scale.y = 12;
+//		transform.localScale = scale;
+
+		WebCamTexture cam = new WebCamTexture();
+		renderer.material.mainTexture = cam;
+		//renderer.material.shader = Shader.Find("Mobile/Unlit (Supports Lightmap)");
+		cam.Play();
+	}
+}
