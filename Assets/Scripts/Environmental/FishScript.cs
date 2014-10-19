e56e362ad52a2c0f90371ab8724da7b745f62147
Tommy <ngo.tommy@outlook.com>
2014-10-20 10:44:29 +1300

    Make Right Fish and Left Fish

--- Assets/Scripts/Environmental/FishScript.cs
+++ Assets/Scripts/Environmental/FishScript.cs
@@ -5,34 +5,27 @@ using System;
 public class FishScript : MonoBehaviour {
 
 	private const int GRAVITY_SCALE = 2;
-	private bool isDown;
-	private float beginX = -0.5f;
-	private float finalX = -1.5f;
-	private float beginTorque = -0.8f;
-	private float finalTorque = -7;
+	private bool isDown; // if the fish reaches maximum heigh ==> isDown is true
+	public int height = 4;// the maximum height of the fish
+	private float beginX = -0.5f;// variable make the fish move to in the x axis when it starts moving
+	public float finalX = -1.5f;// variable make the fish move to in the x axis when it reaches maximum height
+	private float beginTorque = -0.8f; // variable make the fish spins at the start.
+	public float finalTorque = -7; // varible that make the fish spins when it reaches maximum height
 
 	// Use this for initialization
 	void Start () {
-		// Birds flap their wings when flying
-		var animator = GetComponent<Animator> ();
-		if (animator != null)
-		{
-			animator.enabled = true;
-		}
-		if (transform.localScale.y == 0) {
+
+		if (transform.localRotation.y == 0) {
 			beginX *= -1;
 			finalX *= -1;
-			beginTorque *= -1;
-			finalTorque *= -1;
 		}
-
 
 		// Allow fish start moving
 		rigidbody2D.gravityScale = GRAVITY_SCALE;
 		rigidbody2D.velocity = new Vector3 (0, 10, 0);
 		isDown = false;
 		// Destroy birds when well off offscreen
-		Destroy (gameObject, 5);
+		Destroy (gameObject, 2);
 	}
 	
 	// Update is called once per frame
@@ -40,7 +33,7 @@ public class FishScript : MonoBehaviour {
 
 		Vector3 position = transform.position;
 
-		if (position.y < 4 && !isDown) {
+		if (position.y < height && !isDown) {
 			
 			rigidbody2D.velocity = new Vector3 (beginX, 20, 0);
 			rigidbody2D.AddTorque(beginTorque);
