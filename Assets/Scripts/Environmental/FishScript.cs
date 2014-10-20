541b6fbf94fb0d43caad14880b06757b0e512dea
Tommy <ngo.tommy@outlook.com>
2014-10-20 15:21:08 +1300

    Rework the fish AI.

--- Assets/Scripts/Environmental/FishScript.cs
+++ Assets/Scripts/Environmental/FishScript.cs
@@ -4,46 +4,41 @@ using System;
 
 public class FishScript : MonoBehaviour {
 
-	private const int GRAVITY_SCALE = 2;
-	private bool isDown; // if the fish reaches maximum heigh ==> isDown is true
-	public int height = 4;// the maximum height of the fish
-	private float beginX = -0.5f;// variable make the fish move to in the x axis when it starts moving
-	public float finalX = -1.5f;// variable make the fish move to in the x axis when it reaches maximum height
-	private float beginTorque = -0.8f; // variable make the fish spins at the start.
-	public float finalTorque = -7; // varible that make the fish spins when it reaches maximum height
+	public float distance = 20; // distance fish travels
+	public float height = 10;// the height of the fish jump.
+	private float startTime;
+
+	private Vector3 startPos;
+	private Vector3 endPos;
 
 	// Use this for initialization
 	void Start () {
-
+		startPos = new Vector3 (rigidbody2D.position.x, rigidbody2D.position.y);
+		endPos = startPos;
 		if (transform.localRotation.y == 0) {
-			beginX *= -1;
-			finalX *= -1;
+			endPos.x = rigidbody2D.position.x + distance;
+		}else{
+			endPos.x = rigidbody2D.position.x - distance;
 		}
+		height -= rigidbody2D.position.y;
 
-		// Allow fish start moving
-		rigidbody2D.gravityScale = GRAVITY_SCALE;
-		rigidbody2D.velocity = new Vector3 (0, 10, 0);
-		isDown = false;
-		// Destroy birds when well off offscreen
+		startTime = Time.time;
 		Destroy (gameObject, 2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		Vector3 position = transform.position;
-
-		if (position.y < height && !isDown) {
-			
-			rigidbody2D.velocity = new Vector3 (beginX, 20, 0);
-			rigidbody2D.AddTorque(beginTorque);
-		}else{
-			isDown = true;
-		}
-
-		if (isDown) {
-			rigidbody2D.AddTorque(finalTorque);
-			rigidbody2D.velocity = new Vector3 (finalX, -10, 0);
-		}
+		float cTime = (Time.time - startTime)* 0.5f;
+		Vector3 currentPos = Vector3.Lerp(startPos, endPos, cTime);
+		float angle = rigidbody2D.rotation;
+		Debug.Log (angle);
+		// add a value to Y, using Sine to give a curved trajectory in the Y direction
+		currentPos.y += height * Mathf.Sin(Mathf.Clamp01(cTime) * Mathf.PI);
+		
+		// finally assign the computed position to our gameObject:
+		transform.position = currentPos;
+		// add torque to the fish to make it look like turning.
+		transform.RotateAround(currentPos, new Vector3(0,0,1),2.5f * 0.5f);
+	
 	}
 }
