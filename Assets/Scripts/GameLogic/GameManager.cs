1b29da86466b35165e86d4c59a9bfea0274bb8bd
Chris Morgan <chris.j.r.morgan@gmail.com>
2014-10-21 11:04:51 +1300

    added some skeleton code for game logic

--- Assets/Scripts/GameLogic/GameManager.cs
+++ Assets/Scripts/GameLogic/GameManager.cs
@@ -23,6 +23,8 @@ public class GameManager : GooglePlayGames.BasicApi.OnStateLoadedListener {
     private static GameManager sInstance = new GameManager();
     private int mLevel = 0;
 
+    private GameProgress mProgress;
+
     private bool mAuthenticating = false;
     private string mAuthProgressMessage = Strings.SigningIn;
 
@@ -51,6 +53,7 @@ public class GameManager : GooglePlayGames.BasicApi.OnStateLoadedListener {
     }
 
     private GameManager() {
+        mProgress = GameProgress.LoadFromDisk();
     }
 
     #if UNITY_ANDROID || UNITY_IOS
