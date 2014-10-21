70f96e0cf67b2a013398dff627c7839eea28e6b6
Mark Aziz <mazi910@aucklanduni.ac.nz>
2014-10-21 13:06:51 +1300

    created camera mode toggle logic

--- Assets/Scripts/Interface/CameraToggle.cs
+++ Assets/Scripts/Interface/CameraToggle.cs
@@ -1,29 +1,29 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class CameraToggle : MonoBehaviour {
+public class CameraToggle : ButtonBehaviour {
 
 	public bool cameraMode = false;
-	GUIStyle style;
-	// Use this for initialization
-	void Start () {
-		style = new GUIStyle ();
-		style.fontSize = 30;
-		style.fixedWidth = 500;
-		style.fixedWidth = 500;
-	}
+	public Sprite onButtonSprite;
+	public Sprite offButtonSprite;
 
-	void OnGUI(){
-		cameraMode = GUI.Toggle(new Rect(200, 200, 150, 20), cameraMode,"Camera Mode",style);
-		//GUI.Label (new Rect(230, 200, 150, 20), "Camera Mode",style);
-		//GUI.enabled = cameraMode;
+	protected override void OnButtonPress(){
+		cameraMode = !cameraMode;
+		Debug.Log(cameraMode.ToString());
+
+		if (cameraMode) {
+			PlayerPrefs.SetInt("CameraMode",1);
+		} else {
+			PlayerPrefs.SetInt("CameraMode",0);
+		}
 	}
+	
 
 	void Update(){
-		if (cameraMode) {
-			PlayerPrefs.SetInt("CameraMode", 1);		
+		if (PlayerPrefs.GetInt("CameraMode")==1) {
+			GetComponent<SpriteRenderer> ().sprite = onButtonSprite;
 		} else {
-			PlayerPrefs.SetInt("CameraMode",0);		
+			GetComponent<SpriteRenderer> ().sprite = offButtonSprite;
 		}
 	}
 }
