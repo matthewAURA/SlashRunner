ad3cb16c85dfe057ae8ab48a2404a65256a8f23c
Mark Aziz <mazi910@aucklanduni.ac.nz>
2014-10-21 13:05:28 +1300

    Added new sprites for menus

---
+++ Assets/Scripts/Interface/CameraToggle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraToggle : MonoBehaviour {
+
+	public bool cameraMode = false;
+	GUIStyle style;
+	// Use this for initialization
+	void Start () {
+		style = new GUIStyle ();
+		style.fontSize = 30;
+		style.fixedWidth = 500;
+		style.fixedWidth = 500;
+	}
+
+	void OnGUI(){
+		cameraMode = GUI.Toggle(new Rect(200, 200, 150, 20), cameraMode,"Camera Mode",style);
+		//GUI.Label (new Rect(230, 200, 150, 20), "Camera Mode",style);
+		//GUI.enabled = cameraMode;
+	}
+
+	void Update(){
+		if (cameraMode) {
+			PlayerPrefs.SetInt("CameraMode", 1);		
+		} else {
+			PlayerPrefs.SetInt("CameraMode",0);		
+		}
+	}
+}
