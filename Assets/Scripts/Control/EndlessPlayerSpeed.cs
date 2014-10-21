258c20b1dce526431dd008db62bc515e25a6d4b7
Matthew Dyer <matthew.dyer@aura.org.nz>
2014-10-21 15:04:22 +1300

    refactored speed up to allow for exensibility, endless runner now speeds up

---
+++ Assets/Scripts/Control/EndlessPlayerSpeed.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Platform {
+
+	public class EndlessPlayerSpeed : SpeedUpAvatarScript,PlatformSpawnListener {
+		
+		public PlatformSpawner spawner;
+
+		void Start(){
+			spawner.registerListener (this);
+		}
+
+		public void onPlatformSpawn(Vector3 position){
+			Debug.Log ("speed up!");
+			this.speedUp ();
+		}
+}
+}
