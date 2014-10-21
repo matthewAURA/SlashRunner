93353353b2ccb7c6f69f52cb7bfedfe5844175b6
Mark Aziz <mazi910@aucklanduni.ac.nz>
2014-10-21 15:45:03 +1300

    added camera mode to settings scene

--- Assets/Scripts/Interface/CameraToggle.cs
+++ Assets/Scripts/Interface/CameraToggle.cs
@@ -3,9 +3,20 @@ using System.Collections;
 
 public class CameraToggle : ButtonBehaviour {
 
-	public bool cameraMode = false;
+	public bool cameraMode;
 	public Sprite onButtonSprite;
 	public Sprite offButtonSprite;
+
+	void Start(){
+		Debug.Log (cameraMode);
+		if (PlayerPrefs.GetInt("CameraMode")==1) {
+			cameraMode=true;
+			GetComponent<SpriteRenderer> ().sprite = onButtonSprite;
+		} else {
+			cameraMode=false;
+			GetComponent<SpriteRenderer> ().sprite = offButtonSprite;
+		}
+	}
 
 	protected override void OnButtonPress(){
 		cameraMode = !cameraMode;
@@ -13,17 +24,11 @@ public class CameraToggle : ButtonBehaviour {
 
 		if (cameraMode) {
 			PlayerPrefs.SetInt("CameraMode",1);
-		} else {
-			PlayerPrefs.SetInt("CameraMode",0);
-		}
-	}
-	
-
-	void Update(){
-		if (PlayerPrefs.GetInt("CameraMode")==1) {
 			GetComponent<SpriteRenderer> ().sprite = onButtonSprite;
 		} else {
+			PlayerPrefs.SetInt("CameraMode",0);
 			GetComponent<SpriteRenderer> ().sprite = offButtonSprite;
 		}
 	}
+		
 }
