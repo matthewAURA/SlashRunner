4664b61a46b8b4717ab900164385a63ea00aef9a
Mark Aziz <mazi910@aucklanduni.ac.nz>
2014-10-22 16:01:42 +1300

    changed the winning screen to display proper background and new message

--- Assets/Scripts/Interface/WinningScreen/WinningScript.cs
+++ Assets/Scripts/Interface/WinningScreen/WinningScript.cs
@@ -10,10 +10,10 @@ public class WinningScript : MonoBehaviour {
 
 	void OnGUI(){
 		GUIStyle style = new GUIStyle();
-		style.normal.textColor = Color.blue;
+		style.normal.textColor = Color.black;
 		style.fontSize = 80;
 		style.alignment = TextAnchor.MiddleCenter;
-		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "CONGRATULATION",style);
+		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "Level Completed!",style);
 		style.normal.textColor = Color.white;
 		style.fontSize = 40;
 		GUI.Label( new Rect (Screen.width / 2 - 40, Screen.height/2 - 40, 80, 30), "Score: " + score,style);
